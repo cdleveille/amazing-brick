3d22c40586ffd7580e83d445daca5982847ae3b5
Chris Leveille <cdleveille@gmail.com>
2024-10-10 15:18:02 -0400

    render svgs inline in markup

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -13,63 +13,53 @@ export const Home = () => {
 	} = useAppContext();
 
 	return (
-		<>
-			<div
-				className="home-container"
-				style={{ paddingTop: `${128 * scaleRatio}px`, rowGap: `${72 * scaleRatio}px` }}
-			>
-				<div className="header-container">
-					<h1
-						className="header"
-						style={{
-							fontSize: `${72 * scaleRatio}px`,
-							lineHeight: `${64 * scaleRatio}px`
-						}}
-					>
-						AMAZING BRICK&nbsp;
-					</h1>
-					<Brick
-						style={{
-							position: "absolute",
-							width: `${36 * scaleRatio}px`,
-							height: `${36 * scaleRatio}px`,
-							top: `${76 * scaleRatio}px`,
-							left: `${33 * scaleRatio}px`,
-							backgroundColor: isDarkMode ? Color.White : Color.Black
-						}}
-					/>
-				</div>
-				<div
-					className="game-mode-select"
+		<div className="home-container" style={{ paddingTop: `${128 * scaleRatio}px`, rowGap: `${72 * scaleRatio}px` }}>
+			<div className="header-container">
+				<h1
+					className="header"
 					style={{
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						rowGap: `${16 * scaleRatio}px`
+						fontSize: `${72 * scaleRatio}px`,
+						lineHeight: `${64 * scaleRatio}px`
 					}}
 				>
-					<GameModeMenu value={gameMode} onSelectOption={(gameMode: TGameMode) => setGameMode(gameMode)} />
-					<Text size={18} style={{ color: isDarkMode ? "#cccccc" : "#555555" }}>
-						{gameMode.description}
-					</Text>
-				</div>
-				<div style={{ display: "flex", flexDirection: "column", rowGap: `${32 * scaleRatio}px` }}>
-					<Button onClick={() => setScreen("play")} backgroundColor={Color.Green} autoFocus>
-						<Text size={26}>PLAY</Text>
-					</Button>
-					<Button onClick={() => setScreen("scores")} backgroundColor={Color.Blue}>
-						<Text size={26}>SCORES</Text>
-					</Button>
-					<Button onClick={() => setScreen("rate")} backgroundColor={Color.Red}>
-						<Text size={26}>RATE</Text>
-					</Button>
-				</div>
+					AMAZING BRICK&nbsp;
+				</h1>
+				<Brick
+					style={{
+						position: "absolute",
+						width: `${36 * scaleRatio}px`,
+						height: `${36 * scaleRatio}px`,
+						top: `${76 * scaleRatio}px`,
+						left: `${33 * scaleRatio}px`,
+						backgroundColor: isDarkMode ? Color.White : Color.Black
+					}}
+				/>
 			</div>
-			<div style={{ display: "none" }}>
-				<img src="./assets/jump.svg" alt="jump" />
-				<img src="./assets/thumbs_up.svg" alt="thumbs up" />
-				<img src="./assets/thumbs_down.svg" alt="thumbs down" />
+			<div
+				className="game-mode-select"
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					rowGap: `${16 * scaleRatio}px`
+				}}
+			>
+				<GameModeMenu value={gameMode} onSelectOption={(gameMode: TGameMode) => setGameMode(gameMode)} />
+				<Text size={18} style={{ color: isDarkMode ? "#cccccc" : "#555555" }}>
+					{gameMode.description}
+				</Text>
 			</div>
-		</>
+			<div style={{ display: "flex", flexDirection: "column", rowGap: `${32 * scaleRatio}px` }}>
+				<Button onClick={() => setScreen("play")} backgroundColor={Color.Green} autoFocus>
+					<Text size={26}>PLAY</Text>
+				</Button>
+				<Button onClick={() => setScreen("scores")} backgroundColor={Color.Blue}>
+					<Text size={26}>SCORES</Text>
+				</Button>
+				<Button onClick={() => setScreen("rate")} backgroundColor={Color.Red}>
+					<Text size={26}>RATE</Text>
+				</Button>
+			</div>
+		</div>
 	);
 };
--- src/client/components/Jump.tsx
+++ src/client/components/Jump.tsx
@@ -9,13 +9,39 @@ export const Jump = () => {
 
 	return (
 		<>
-			<object
-				data="./assets/jump.svg"
-				type="image/svg+xml"
+			<svg
 				width={250 * scaleRatio}
+				viewBox="0 0 246.891 117.059"
+				xmlns="http://www.w3.org/2000/svg"
 				className="absolute-center"
-				style={{ marginTop: `${-92 * scaleRatio}px` }}
-			/>
+				style={{ marginTop: `${-90 * scaleRatio}px` }}
+			>
+				<path
+					style={{
+						fill: "#a3a3a3",
+						fillOpacity: 1,
+						strokeWidth: 1.21417,
+						strokeLinejoin: "round"
+					}}
+					transform="rotate(-44.717 -.163 .721)"
+					d="M109.686 192.981h24.229v23.456h-24.229ZM-17.829 66.72l-.239-24.228-23.454.232.239 24.227z"
+				/>
+				<path
+					style={{
+						fill: "none",
+						stroke: "#a3a3a3",
+						strokeWidth: 2.13543,
+						strokeLinecap: "butt",
+						strokeLinejoin: "miter",
+						strokeMiterlimit: 4,
+						strokeDasharray: "4.27087,2.13543",
+						strokeDashoffset: 0,
+						strokeOpacity: 1
+					}}
+					d="M127.895 116.908c.334-9.703 26.424-162.44 85.117-101.704 4.892 5.062 8.45 11.175 12.69 19.84m-105.597 81.864c-.334-9.703-26.423-162.44-85.117-101.704-4.892 5.062-8.45 11.175-12.69 19.84"
+					transform="translate(-.555 .094)"
+				/>
+			</svg>
 			<div
 				className="absolute-center"
 				style={{
